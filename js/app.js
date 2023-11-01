9258609bc3fd87ceebd82e44c0338d3657db311c
ExequielA <alaniz.exequielalejandro@iesconcepcion.edu.ar>
2023-11-01 15:47:43 -0300

    removed the test js file

--- js/app.js
+++ js/app.js
@@ -6,16 +6,17 @@ let spanSegundos = document.getElementById("displaySegundos");
 let spanMinutos = document.getElementById("displayMinutos");
 let spanHoras = document.getElementById("displayHoras");
 
-let buttonIniciar = document.getElementById("btnIniciar");
-let buttonLap = document.getElementById("btnLap");
-let buttonDetener = document.getElementById("btnDetener");
+let btnIniciar = document.getElementById("btnIniciar");
+let btnLap = document.getElementById("btnLap");
+let btnDetener = document.getElementById("btnDetener");
+let btnReiniciar = document.getElementById("btnReiniciar");
 
-let idIntervalo
+let listaUl = document.getElementById("listaUl");
 
-let condicional = false
+let idIntervalo;
+let intervaloFuncionando = false;
 
-function displayCronometro() {
-    window.setInterval(()=> {
+const displayCronometro = () => {
         segundos+=1;
         if(segundos == 60){
             segundos = 0;
@@ -25,21 +26,77 @@ function displayCronometro() {
                 horas++;
             }
         }
-    }, 1000)
-};
+    };
 
-function aplicarCambios() {window.setInterval(()=> {
+const aplicarCambios = () => {
     spanSegundos.innerText = segundos;
     spanMinutos.innerText = minutos;
-    spanHoras.innerText = horas}, 1000)
+    spanHoras.innerText = horas;
+};
+
+const iniciarIntervalo = () => {
+    idIntervalo = setInterval( ()=> {
+        console.log("Funcionando");
+        funcionesInicio();
+    }, 1000)
+    intervaloFuncionando = true;
+    actualizarBtnReiniciar();
+    btnDetener.disabled = false;
+    btnIniciar.disabled = true;
+    btnLap.disabled = false;
 }
 
-function llamarDosFunciones() {
+const detenerIntervalo = () => {
+    clearInterval(idIntervalo);
+    console.log("Intervalo detenido");
+    intervaloFuncionando = false;
+    actualizarBtnReiniciar()
+    btnDetener.disabled = true;
+    btnIniciar.disabled = false;
+    btnLap.disabled = true;
+}
+
+const funcionesInicio = () => {
     displayCronometro();
     aplicarCambios();
 }
 
-buttonIniciar.addEventListener("click", llamarDosFunciones())
+const lapTiempo = () => {
+    const tiempoLapeado = document.createElement("li");
+    tiempoLapeado.textContent = `${horas}:${minutos}:${segundos}`;
+    listaUl.appendChild(tiempoLapeado);
+}
 
+const actualizarBtnReiniciar = () => {
+    if (intervaloFuncionando) {
+    btnReiniciar.disabled = intervaloFuncionando;
+    btnReiniciar.style.visibility = "hidden";
+    } else {
+        btnReiniciar.disabled = intervaloFuncionando;
+        btnReiniciar.style.visibility = "visible";
+    }
+}
 
+const limpiarDisplay = () => {
+    segundos = 0;
+    minutos = 0;
+    horas = 0;
+    aplicarCambios();
+    btnReiniciar.disabled = intervaloFuncionando;
+    btnReiniciar.style.visibility = "hidden";
+}
+
+const limpiarLaps = () => {
+    listaUl.innerHTML = "";
+}
+
+const funcionesReiniciar = () => {
+    limpiarDisplay();
+    limpiarLaps();
+}
+
+btnIniciar.addEventListener("click", iniciarIntervalo);
+btnDetener.addEventListener("click", detenerIntervalo);
+btnLap.addEventListener("click", lapTiempo);
+btnReiniciar.addEventListener("click", funcionesReiniciar);
 
